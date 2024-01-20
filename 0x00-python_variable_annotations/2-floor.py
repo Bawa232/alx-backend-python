aa273f2e2d7523e79930b92eb082316d94dbae04
James Bawa <jbawa032@gmail.com>
2024-01-20 13:40:09 +0000

    all good 1

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 ''' python type annotation '''
 
-
 import math
 
 
-def floor(n: float):
+def floor(n: float) -> int:
     ''' returns floor of the float '''
     return math.floor(n)
