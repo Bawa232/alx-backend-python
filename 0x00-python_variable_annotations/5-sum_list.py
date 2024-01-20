49a418d1d95ad7803f7550a02248d3f4fc1131a0
James Bawa <jbawa032@gmail.com>
2024-01-20 18:45:14 +0000

    add

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' complex types '''
+
+
+def sum_list(input_list: list[float]) ->:
+    ''' fn that sums fkoats in a list '''
+    add = 0
+    for num in input_list:
+        add += num
+
+    return add
