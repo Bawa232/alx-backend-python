c72cf44c76b22bacd94006ac21a82009fc3c680a
James Bawa <jbawa032@gmail.com>
2024-01-20 19:06:00 +0000

    add

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' complex types '''
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    ''' returns the sum of the list as float '''
+
+    return sum(mxd_lst)
