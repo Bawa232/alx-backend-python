80e67e1870835c277b03e3230edb3281740bd904
James Bawa <jbawa032@gmail.com>
2024-01-28 20:32:10 +0000

    Async 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,10 +11,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     ''' returns delay times '''
     lis_t = []
+    waits = []
     i = 0
 
     while i < n:
         list_t.append(wait_random(max_delay))
         i += 1
 
-    return sorted(list_t)
+    for future in asyncio.as_completed(list_t):
+        result = await future
+        waits.append(result)
+
+    return waits
