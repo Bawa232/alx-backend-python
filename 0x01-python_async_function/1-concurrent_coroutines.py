df91bed6107ded514b626a2055adee6c62c69418
James Bawa <jbawa032@gmail.com>
2024-01-28 20:12:14 +0000

    Asymc 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' python async programming '''
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    ''' returns delay times '''
+    lis_t = []
+    i = 0
+
+    while i < n:
+        list_t.push(wait_random(max_delay))
+
+    return sorted(list_t)
